4872a9e311d90e8b233542c3bba25c5dbccaf62f
koseki <kosekitau@gmail.com>
2020-06-25 15:24:53 +0900

    BiLSTMモデルの訓練からテストデータでの評価を行うところまで

--- CBET/cbet_bilstm.py
+++ CBET/cbet_bilstm.py
@@ -16,12 +16,64 @@ import torch.optim as optim
 #gpuの確認
 print(torch.cuda.is_available())
 
+#学習済みの分散表現をロードする
+from torchtext.vocab import Vectors
+
+english_fasttext_vectors = Vectors(name='drive/My Drive/wiki-news-300d-1M.vec')
+
+print(english_fasttext_vectors.dim)
+print(len(english_fasttext_vectors.itos))
+
+import string
+import re
+
+# 以下の記号はスペースに置き換えます（カンマ、ピリオドを除く）。
+# punctuationとは日本語で句点という意味です
+print("区切り文字：", string.punctuation)
+# !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+
+# 前処理
+
+
+def preprocessing_text(text):
+    # 改行コードを消去
+    text = re.sub('<br />', '', text)
+
+    # カンマ、ピリオド以外の記号をスペースに置換
+    for p in string.punctuation:
+        if (p == ".") or (p == ","):
+            continue
+        else:
+            text = text.replace(p, " ")
+
+    # ピリオドなどの前後にはスペースを入れておく
+    text = text.replace(".", " . ")
+    text = text.replace(",", " , ")
+    return text
+
+# 分かち書き（今回はデータが英語で、簡易的にスペースで区切る）
+
+
+def tokenizer_punctuation(text):
+    return text.strip().split()
+
+
+# 前処理と分かち書きをまとめた関数を定義
+def tokenizer_with_preprocessing(text):
+    text = preprocessing_text(text)
+    ret = tokenizer_punctuation(text)
+    return ret
+
+
+# 動作を確認します
+print(tokenizer_with_preprocessing('I like cats+'))
+
 import torchtext
 from torchtext.data.utils import get_tokenizer
 
 #テキストに処理を行うFieldを定義
 #fix_lengthはtokenの数
-TEXT = torchtext.data.Field(sequential=True, use_vocab=True, tokenize=get_tokenizer('spacy'),
+TEXT = torchtext.data.Field(sequential=True, use_vocab=True, tokenize=tokenizer_with_preprocessing,
                             lower=True, include_lengths=True, batch_first=True, fix_length=37)
 
 LABEL = torchtext.data.Field(sequential=False, use_vocab=False)
@@ -31,17 +83,10 @@ train_ds, val_ds, test_ds = torchtext.data.TabularDataset.splits(
     path='drive/My Drive/dataset/CBET/ekman', train='train.csv', validation='val.csv', 
     test='test.csv', format='csv', fields=[('Text', TEXT), ('Label', LABEL)])
 
-from torchtext.vocab import Vectors
-
-english_fasttext_vectors = Vectors(name='drive/My Drive/wiki-news-300d-1M.vec')
-
-print(english_fasttext_vectors.dim)
-print(len(english_fasttext_vectors.itos))
-
 #ボキャブラリを作成する
 TEXT.build_vocab(train_ds, vectors=english_fasttext_vectors)
 
-print(TEXT.vocab.stoi)
+print(len(TEXT.vocab.stoi))
 
 batch_size = 64
 d_model = 300
@@ -74,16 +119,18 @@ class Embedder(nn.Module):
 
 
 class LSTM_Layer(nn.Module):
-  def __init__(self, d_model, hidden_size):
+  def __init__(self, d_model, hidden_size, dropout_rate):
     super().__init__()
     self.hidden_size = hidden_size
     self.lstm = nn.LSTM(d_model, hidden_size, batch_first=True, bidirectional=True)
+    self.dropout = nn.Dropout(dropout_rate)
 
   #入力と(h, c)のタプル
   def forward(self, x):
     #[batch_size, ntoken, hidden_size*2], ([2, batch_size, hidden_size], [2, batch_size, hidden_size])
     output, (hn, cn) = self.lstm(x)
     output = torch.cat([hn[i, : ,:] for i in range(hn.shape[0])], dim=1) #[batch_size, hidden_size*2]
+    output = self.dropout(output)
     return output, (hn, cn)
 
   
@@ -103,7 +150,7 @@ class LSTM_Classification(nn.Module):
     super().__init__()
     self.hidden_size = hidden_size
     self.net1 = Embedder(text_embedding_vectors, dropout_rate)
-    self.net2 = LSTM_Layer(d_model, hidden_size)
+    self.net2 = LSTM_Layer(d_model, hidden_size, dropout_rate)
     self.net3 = ClassificationHead(hidden_size, output_dim)
 
   def forward(self, x):
@@ -129,4 +176,111 @@ x1 = net(x)
 
 print("入力のテンソルサイズ：", x.shape)
 print("出力のテンソルサイズ：", x1.shape)
+
+dataloaders_dict = {'train': train_dl, 'val': val_dl}
+criterion = nn.CrossEntropyLoss() 
+net = LSTM_Classification(TEXT.vocab.vectors, d_model, hidden_size, output_dim, dropout_rate) 
+net.train()
+
+learning_rate = 10e-4
+#optimizer = optim.SGD(net.parameters(), lr=learning_rate)
+optimizer = optim.Adam(net.parameters(), lr=learning_rate)
+
+def train_model(net, dataloaders_dict, criterion, optimizer, num_epochs):
+  device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+  net.to(device)
+
+  torch.backends.cudnn.benchmark = True
+
+  #各epoch
+  for epoch in range(num_epochs):
+    #訓練と評価
+    for phase in ['train', 'val']:
+      if phase == 'train':
+        net.train()
+      else:
+        net.eval()
+      
+      epoch_loss = 0.0 #各epochの損失の和
+      epoch_corrects = 0 #各epochの正解数
+
+      for batch in (dataloaders_dict[phase]):
+        inputs = batch.Text[0].to(device)
+        labels = batch.Label.to(device)
+
+        optimizer.zero_grad()
+        
+        with torch.set_grad_enabled(phase=='train'):
+          #hidden = net.init_hidden(device) #LSTM隠れ状態の初期化
+          outputs = net(inputs) #[batch_size, output_dim]
+
+          loss = criterion(outputs, labels) #softmaxは中に入ってる
+          _, preds = torch.max(outputs, 1)
+
+          if phase == 'train':
+            loss.backward() #勾配を計算
+            optimizer.step() #パラメータを更新
+
+          epoch_loss += loss.item()*inputs.size(0) #バッチ数をかけてあとでデータ量で割る
+          epoch_corrects += torch.sum(preds == labels.data)
+      
+      #各epochのloss、正解数をだす
+      epoch_loss = epoch_loss/len(dataloaders_dict[phase].dataset)
+      epoch_acc = epoch_acc = epoch_corrects.double()/len(dataloaders_dict[phase].dataset)
+      print('Epoch {}/{} | {:^5} |  Loss: {:.4f} Acc: {:.4f}'.format(epoch+1,
+                                                                     num_epochs, phase, epoch_loss, epoch_acc))
+  return net
+
+num_epochs = 20
+net_trained = train_model(net, dataloaders_dict,
+                          criterion, optimizer, num_epochs=num_epochs)
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+net_trained.eval()
+net_trained.to(device)
+
+y_true = np.array([])
+y_pred = np.array([])
+
+epoch_corrects = 0
+
+for batch in (test_dl):
+  inputs = batch.Text[0].to(device)
+  labels = batch.Label.to(device)
+
+  with torch.set_grad_enabled(False):
+    outputs = net_trained(inputs)
+    _, preds = torch.max(outputs, 1)
+    
+    y_true = np.concatenate([y_true, labels.to("cpu", torch.double).numpy()])
+    y_pred = np.concatenate([y_pred, preds.to("cpu", torch.double).numpy()])
+
+    epoch_corrects += torch.sum(preds == labels.data)
+
+# 正解率
+epoch_acc = epoch_corrects.double() / len(test_dl.dataset)
+
+print('テストデータ{}個での正解率：{:.4f}'.format(len(test_dl.dataset),epoch_acc))
+
+from sklearn.metrics import classification_report
+
+print(classification_report(y_true, y_pred))
+
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+
+confmat = confusion_matrix(y_true=y_true, y_pred=y_pred)
+fig, ax = plt.subplots(figsize=(2.5, 2.5))
+ax.matshow(confmat, cmap=plt.cm.Blues, alpha=0.3)
+for i in range(confmat.shape[0]):
+    for j in range(confmat.shape[1]):
+        ax.text(x=j, y=i, s=confmat[i, j], va='center', ha='center')
+
+plt.xlabel('Predicted label')
+plt.ylabel('True label')
+
+plt.tight_layout()
+#plt.savefig('confusion_matrix.png', dpi=300)
+plt.show()
 
